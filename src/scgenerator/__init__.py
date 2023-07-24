a85ad8ac23a162ab90682db5202521edfa32efd1
Benoît Sierro <benoit.sierro@bluewin.ch>
2023-07-24 16:31:03 +0200

    fixed metadata

--- src/scgenerator/__init__.py
+++ src/scgenerator/__init__.py
@@ -6,4 +6,4 @@ from scgenerator.math import abs2, argclosest, normalized, span, tspace, wspace
 from scgenerator.parameter import Parameters
 from scgenerator.physics import fiber, materials, plasma, pulse, units
 from scgenerator.physics.units import PlotRange
-from scgenerator.solver import integrate, solve43
+from scgenerator.solver import integrate, solve43, SimulationResult
